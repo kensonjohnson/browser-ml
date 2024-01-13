b5d7c394bbfb0203d39fe6ea9773d834ae9314f4
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-12 20:00:23 -0600

    Prepare for deployment to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { resolve, join } from "node:path";
 import { cwd } from "node:process";
 import { globSync } from "glob";
 
+let baseUrl = "/";
+
+if (process.env.NODE_ENV === "production") {
+  baseUrl = "/" + (cwd().split("/").pop() || "");
+}
+
 export default defineConfig({
+  base: baseUrl,
   root: join(cwd(), "src"),
   publicDir: join(cwd(), "public"),
   build: {
