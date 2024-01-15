af508191543b4b6e929a84a1d389b01870639313
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-15 09:29:44 -0600

    Swapped out C style loop with modern for-of loop

--- src/object-recognition/object-recognition.ts
+++ src/object-recognition/object-recognition.ts
@@ -78,27 +78,27 @@ function predictWebcam() {
 
     // Now lets loop through predictions and draw them to the live view if
     // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
+    for (const prediction of predictions) {
       // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
+      if (prediction.score > 0.66) {
         const p = document.createElement("p");
         p.innerText =
-          predictions[n].class +
+          prediction.class +
           " - with " +
-          Math.round(predictions[n].score * 100) +
+          Math.round(prediction.score * 100) +
           "% confidence.";
         p.setAttribute(
           "style",
-          `margin-left: ${predictions[n].bbox[0]}px; margin-top: ${
-            predictions[n].bbox[1] - 10
-          }px; width: ${predictions[n].bbox[2] - 10}px; top: 0; left: 0;`
+          `margin-left: ${prediction.bbox[0]}px; margin-top: ${
+            prediction.bbox[1] - 10
+          }px; width: ${prediction.bbox[2] - 10}px; top: 0; left: 0;`
         );
 
         const highlighter = document.createElement("div");
         highlighter.setAttribute("class", "highlighter");
         highlighter.setAttribute(
           "style",
-          `left: ${predictions[n].bbox[0]}px; top: ${predictions[n].bbox[1]}px; width: ${predictions[n].bbox[2]}px; height: ${predictions[n].bbox[3]}px;`
+          `left: ${prediction.bbox[0]}px; top: ${prediction.bbox[1]}px; width: ${prediction.bbox[2]}px; height: ${prediction.bbox[3]}px;`
         );
 
         liveView.appendChild(highlighter);
